b3969fb0169d259e57361595278e1e2a3ee97372
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-06-30 23:58:58 +0100

    Added Repository Pattern for making HTTP calls

---
+++ Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(string url, int Id);
+        Task<IEnumerable<T>> GetAllAsync(string url);
+        Task<bool> CreateAsync(string url, T objToCreate);
+        Task<bool> UpdateAsync(String url, T objToUpdate);
+        Task<bool> DeleteAsync(String url, int Id);
+    }
+}
---
+++ SD.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ParkyWeb
+{
+    public class SD
+    {
+        public static string APIBaseURL = "https://localhost:14792/";
+        public static string NationalParkAPIPath = APIBaseURL + "api/v1/nationalparks";
+        public static string TrailsAPIPath = APIBaseURL + "api/v1/trails";
+    }
+}
