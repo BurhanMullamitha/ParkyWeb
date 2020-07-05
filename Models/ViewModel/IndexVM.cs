664ed06e5bd2a57dfba3dd8454b80565b3c7d8bd
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-07-05 02:51:22 +0100

    Added View Model and Controller for Homepage

---
+++ Models/ViewModel/IndexVM.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ParkyWeb.Models.ViewModel
+{
+  public class IndexVM
+  {
+    public IEnumerable<NationalPark> NationalParkList { get; set; }
+    public IEnumerable<Trail> TrailList { get; set; }
+  }
+}
