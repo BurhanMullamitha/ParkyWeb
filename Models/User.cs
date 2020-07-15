553d5030483f10ce8c730f8f25cbc880e838e729
Burhan Mullamitha <burhanmm1998@gmail.com>
2020-07-15 13:39:04 +0100

    Added User model and Account Repository for login/register users

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParkyWeb.Models
+{
+  public class User
+  {
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+    public string Token { get; set; }
+  }
+}
